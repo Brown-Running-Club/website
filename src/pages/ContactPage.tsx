309953b04e40fd2e35c07a811918af21c52b60a3
Julia McClellan <juliamcclellan37@gmail.com>
2021-05-06 12:10:27 -0400

    Update contact form iframe

--- src/pages/ContactPage.tsx
+++ src/pages/ContactPage.tsx
@@ -9,7 +9,7 @@ export default class ContactPage extends React.Component {
         <NavigationBar />
         <PageBody>
             <div>
-        <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSfZwHmQDJBEbp__naoK5t5Ua7ql2xlXOBj_f7rfokcZW0H5WA/viewform?embedded=true" width="640" height="592" marginheight="0" marginwidth="0">Loading…</iframe>
+        <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSfZwHmQDJBEbp__naoK5t5Ua7ql2xlXOBj_f7rfokcZW0H5WA/viewform?embedded=true" width="640" height="592">Loading…</iframe>
         </div>
           </PageBody>
       </>
