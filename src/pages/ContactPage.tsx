fa0a9f15e31ed846691549cdc6ee84a0762d2ddc
Julia McClellan <juliamcclellan37@gmail.com>
2021-05-06 12:12:36 -0400

    Iframe title

--- src/pages/ContactPage.tsx
+++ src/pages/ContactPage.tsx
@@ -9,7 +9,7 @@ export default class ContactPage extends React.Component {
         <NavigationBar />
         <PageBody>
             <div>
-        <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSfZwHmQDJBEbp__naoK5t5Ua7ql2xlXOBj_f7rfokcZW0H5WA/viewform?embedded=true" width="640" height="592">Loading…</iframe>
+        <iframe title="contact" src="https://docs.google.com/forms/d/e/1FAIpQLSfZwHmQDJBEbp__naoK5t5Ua7ql2xlXOBj_f7rfokcZW0H5WA/viewform?embedded=true" width="640" height="592">Loading…</iframe>
         </div>
           </PageBody>
       </>
