f4322afa3048eee477b91f81311d58d3834987fc
Julia McClellan <juliamcclellan37@gmail.com>
2021-05-09 07:48:00 -0400

    start of meet schedule

---
+++ src/components/season.tsx
@@ -0,0 +1,74 @@
+import React from "react"
+import theme from "../config/theme"
+
+type Meet = {
+  race: string
+  description?: string
+  date: Date
+  link?: string
+  location: string
+}
+
+type Schedule = Meet[]
+
+const SHEET_ID = "1hvbRCCS-jP2bxBD9HoRZ3bKetfV-LiKb-ZT8a7Y5-NU"
+const RANGE = "A2:E100"
+const API_KEY = "AIzaSyB_4qRtTbmqHERuJLpTJAWOjkFwx4c2zMo"
+
+async function getSeason(season) {
+  const url =
+    "https://sheets.googleapis.com/v4/spreadsheets/" +
+    SHEET_ID +
+    "/values/" +
+    encodeURIComponent(season + "!" + RANGE) +
+    "?key=" +
+    API_KEY
+  return await fetch(url)
+    .then(res => res.json())
+    .then(res => res.values)
+    .then(meets => meets.map(meet => ({
+      date: meet[0],
+      race: meet[1],
+      location: meet[2],
+      description: meet[3],
+      url: meet[4],
+    })))
+}
+
+export default class Season extends React.Component<{ season: string }, { schedule?: Schedule }> {
+  renderMeet(meet: Meet) {
+    return (
+      <tr>
+        <td>{meet.date}</td>
+        <td>{
+          meet.link === undefined
+            ? meet.race
+            : (<a href={meet.link}>meet.race</a>)
+        }</td>
+        <td>{meet.location}</td>
+      </tr>
+    )
+  }
+
+  render() {
+    const schedule = this.state?.schedule
+    let meets: JSX.Element[] = []
+    if (schedule) {
+      schedule.forEach(meet => meets.push(this.renderMeet(meet)))
+    }
+    return (
+      <table>
+        <tr>
+          <th>Date</th>
+          <th>Meet</th>
+          <th>Location</th>
+        </tr>
+        {meets}
+      </table>
+    )
+  }
+
+  componentDidMount() {
+    getSeason(this.props.season).then(schedule => this.setState({ schedule: schedule }))
+  }
+}
---
+++ src/pages/schedule.tsx
@@ -0,0 +1,53 @@
+import React from "react"
+import TitleImage from "../components/title-image"
+import CompetitiveImage from "../images/competitive.jpg"
+import theme from "../config/theme"
+import PageBody from "../components/page-body"
+import WideContainer from "../components/wide-container"
+import Card from "../components/card"
+import NarrowContainer from "../components/narrow-container"
+import Button from "../components/button"
+import Layout from "../components/layout"
+import Season from "../components/season"
+
+export default () => (
+  <Layout title="Schedule">
+    <TitleImage image={CompetitiveImage}>
+      <p style={styles.titleText}>SCHEDULE</p>
+    </TitleImage>
+    <PageBody>
+      <WideContainer>
+        <Card>
+          <Season season="Fall 2021" />
+        </Card>
+      </WideContainer>
+      <NarrowContainer>
+        <Card title="Previous" centeredTitle>
+          <Button
+            leftAlign
+            text="Past Meet Schedules and Results ➞"
+            link="/previous-seasons"
+          />
+        </Card>
+      </NarrowContainer>
+    </PageBody>
+  </Layout>
+)
+
+const styles = {
+  titleText: {
+    ...theme.typography.title,
+    color: theme.palette.white,
+    marginTop: theme.spacing.unit * 2,
+    textShadow: "0 0 15px #000000, 0 0 10px #000000",
+  },
+  infoText: {
+    ...(theme.typography.h4 as any),
+    margin: 0,
+    marginBottom: theme.spacing.unit * 2,
+  },
+  infoTextBottom: {
+    ...(theme.typography.h4 as any),
+    margin: 0,
+  },
+}
