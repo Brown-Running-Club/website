553a9f90bf139593f0048aebd95d792c486295d8
Max Heller <max.a.heller@gmail.com>
2021-09-12 17:58:01 -0400

    remove png course maps

--- src/pages/brown-bear-invitational.tsx
+++ src/pages/brown-bear-invitational.tsx
@@ -7,8 +7,8 @@ import WideContainer from "../components/wide-container"
 import Photo1 from "../images/brown-bear-invitational/course-photo-1.jpg"
 import Photo2 from "../images/brown-bear-invitational/course-photo-2.jpg"
 import Photo3 from "../images/brown-bear-invitational/course-photo-3.jpg"
-import Map6k from "../images/brown-bear-invitational/6k.png"
-import Map8k from "../images/brown-bear-invitational/8k.png"
+import Map6k from "../images/brown-bear-invitational/6k.jpg"
+import Map8k from "../images/brown-bear-invitational/8k.jpg"
 import Layout from "../components/layout"
 
 export default () => (
@@ -100,14 +100,14 @@ export default () => (
           <img style={styles.image} src={Map8k} alt="Highland Park 8k XC Course"></img>
           <br />
           <div style={styles.videoContainer}>
-          <iframe
-            style={styles.video}
-            src="https://www.youtube.com/embed/yRjvYiw_eNs"
-            title="Highland Park 5k XC course recording"
-            frameBorder="0"
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-            allowFullScreen
-          ></iframe>
+            <iframe
+              style={styles.video}
+              src="https://www.youtube.com/embed/yRjvYiw_eNs"
+              title="Highland Park 5k XC course recording"
+              frameBorder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+              allowFullScreen
+            ></iframe>
           </div>
         </Card>
       </WideContainer>
