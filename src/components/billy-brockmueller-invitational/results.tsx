305bde24f355a812752c0333ee05c669e3892ce8
Julia McClellan <juliamcclellan37@gmail.com>
2022-02-03 21:00:26 -0500

    update results

--- src/components/billy-brockmueller-invitational/results.tsx
+++ src/components/billy-brockmueller-invitational/results.tsx
@@ -2,14 +2,9 @@ import React from "react"
 import Card from "../card";
 
 export default () =>
-    <>
-        <Card title="2020">
-            <a href="https://tfrrs.org/results/63479/">Compiled</a>
-        </Card>
-        <Card title="2019">
-            <a href="/brown-bear-invitational/results/2019.pdf">Compiled</a>
-        </Card>
-        <Card title="2018">
-            <a href="/brown-bear-invitational/results/2018.pdf">Compiled</a>
-        </Card>
-    </>
+    <ul>
+        <li><a href="">Meet Records</a></li>
+        <li><a href="https://tfrrs.org/results/63479/">2020</a></li>
+        <li><a href="/brown-bear-invitational/results/2019.pdf">2019</a></li>
+        <li><a href="/brown-bear-invitational/results/2018.pdf">2018</a></li>
+    </ul>
