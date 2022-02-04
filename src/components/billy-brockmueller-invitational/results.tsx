a90f199c0618dbcd0e16e4d3407638469ef268bd
Julia McClellan <juliamcclellan37@gmail.com>
2022-02-03 20:37:20 -0500

    billy brockmueller past results

---
+++ src/components/billy-brockmueller-invitational/results.tsx
@@ -0,0 +1,15 @@
+import React from "react"
+import Card from "../card";
+
+export default () =>
+    <>
+        <Card title="2020">
+            <a href="https://tfrrs.org/results/63479/">Compiled</a>
+        </Card>
+        <Card title="2019">
+            <a href="/brown-bear-invitational/results/2019.pdf">Compiled</a>
+        </Card>
+        <Card title="2018">
+            <a href="/brown-bear-invitational/results/2018.pdf">Compiled</a>
+        </Card>
+    </>
