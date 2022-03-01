9c66f918a7cf6ad93551c548be2245c74eef376c
Julia McClellan <juliamcclellan37@gmail.com>
2022-03-01 11:32:56 -0500

    Records update (#47)

--- src/components/records.tsx
+++ src/components/records.tsx
@@ -1,40 +1,84 @@
 import React, { useEffect, useState } from "react"
 import theme from "../config/theme"
-import { GSheetsTable } from "../components/table"
+import { getSheetData } from "../api-calls"
+import Table from "../components/table"
 import Card from "../components/card"
 
 const SHEET_ID = "1-0FhSZemh9iVF5bEOnqRGIH3ZFAHb9-ktbw5I2Zz9eE"
 
+type Record = {
+  event: string
+  name: string
+  time: string
+  year: string
+  race: string
+  link?: string
+  current?: string
+}
+
 type RecordTables = {
-  men: JSX.Element
-  women: JSX.Element
+  men: Record[]
+  women: Record[]
 }
 
-function getRecords(raceType: string): RecordTables {
-  return {
-    men: getRecordsForGender(raceType, "Men"),
-    women: getRecordsForGender(raceType, "Women"),
-  }
+async function getRecords(raceType: string): RecordTables {
+  const records = [getRecordsForGender(raceType, "Men"), getRecordsForGender(raceType, "Women")];
+  const [men, women] = await Promise.all(records);
+  return { men, women }
 }
 
-function getRecordsForGender(raceType: string, gender: string): JSX.Element {
+async function getRecordsForGender(raceType: string, gender: string): JSX.Element {
   const sheetName = raceType + " - " + gender;
-  return GSheetsTable({ sheetId: SHEET_ID, range: sheetName, fontsize: 15, padding: 8 });
+  return await getSheetData(SHEET_ID, sheetName)
+    .then((records) => records.slice(1).map(record => ({
+      event: record[0],
+      name: record[1],
+      time: record[2],
+      year: record[3],
+      race: record[4],
+      link: record.length > 5 ? record[5] : null,
+      current: record.length > 6 ? record[6] : null
+    })));
+}
+
+function createRecordsTable(records: Record[]) {
+  const headers = ["Event", "Name", "Time", "Year", "Race"];
+  const data = [];
+  for (const r of records) {
+    const race = <>{
+      r.link === undefined
+        ? r.race
+        : (<a href={r.link}>{r.race}</a>)
+    }</>;
+
+    let row = [<>{r.event}</>, <>{r.name}</>, <>{r.time}</>, <>{r.year}</>, race];
+    // bold results from the current year, as listed in the sheet
+    if (r.current !== undefined && r.current === "yes") {
+       row = row.map(cell => <b>{cell}</b>);
+    }
+    data.push(row);
+  }
+  return Table({ header: headers, body: data, fontsize: 15, padding: 8 });
 }
 
 const Records = ({ raceType }: { raceType: string }) => {
-  const records = getRecords(raceType);
+  const [records, setRecords] = useState<RecordData | undefined>(undefined);
+
+  useEffect(() => {
+    if (records === undefined) getRecords(raceType).then(setRecords)
+  })
+
   return (
     <Card title={raceType}>
       <p style={styles.summaryText}>
         <b>Women</b>
       </p>
-      {records.women}
+      {createRecordsTable(records?.women ?? [])}
       <br />
       <p style={styles.summaryText}>
         <b>Men</b>
       </p>
-      {records.men}
+      {createRecordsTable(records?.men ?? [])}
     </Card>
   )
 }
--- src/pages/billy-brockmueller-invitational/records.tsx
+++ src/pages/billy-brockmueller-invitational/records.tsx
@@ -1,17 +1,48 @@
 import React from "react"
 import Omac from "../../images/omac.jpg"
 import theme from "../../config/theme"
+import Card from "../../components/card"
 import PageBody from "../../components/page-body"
 import WideContainer from "../../components/wide-container"
 import Layout from "../../components/layout"
-import Records from "../../components/records"
+import { GSheetsTable } from "../../components/table"
+
+const SHEET_ID = "1n0Juq4mnMg3nU94fI3OPxBRd3fhXLD0Z_AlshD5hm0Q";
+
+function getTable(name: string) {
+  return (
+    <>
+      <p style={styles.summaryText}>
+        <b>{name}</b>
+      </p>
+      <GSheetsTable
+        sheetId={SHEET_ID}
+        range={name}
+        fontsize={15}
+        padding={8}
+      />
+      <br />
+    </>
+  );
+}
 
 export default () => (
   <Layout title="Billy Brockmueller Invitational Records" image={Omac}>
     <PageBody>
       <WideContainer>
-        <Records raceType="Billy Brockmueller Invitational" />
+        <Card title="Billy Brockmueller Invitational">
+          {getTable("Women")}
+          {getTable("Men")}
+        </Card>
       </WideContainer>
     </PageBody>
   </Layout>
 )
+
+const styles = {
+  summaryText: {
+    ...(theme.typography.h4 as any),
+    margin: 0,
+    textAlign: "left" as "left",
+  },
+}
