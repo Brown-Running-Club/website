58725e95d2c9ea4785b0eed80893c71951021001
Max Heller <max_heller@brown.edu>
2022-03-11 14:37:23 -0500

    Link to live results (#50)

--- src/components/billy-brockmueller-invitational/results.tsx
+++ src/components/billy-brockmueller-invitational/results.tsx
@@ -4,6 +4,7 @@ import Card from "../card";
 export default () =>
     <ul style={{ listStyleType: "none" }}>
         <li><a href="/billy-brockmueller-invitational/records">Meet Records</a></li>
+        <li><a href="http://milesplit.live/meets/465677">2022 Live Results</a></li>
         <li><a href="https://tfrrs.org/results/63479/">2020</a></li>
         <li><a href="/billy-brockmueller-invitational/results/2019.pdf">2019</a></li>
         <li><a href="/billy-brockmueller-invitational/results/2018.pdf">2018</a></li>
