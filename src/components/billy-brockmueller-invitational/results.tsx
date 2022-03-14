e36bd6964a3fccbeb78faa54566caf466f9dc46e
Max Heller <max_heller@brown.edu>
2022-03-13 21:47:54 -0400

    Update results.tsx

--- src/components/billy-brockmueller-invitational/results.tsx
+++ src/components/billy-brockmueller-invitational/results.tsx
@@ -4,7 +4,7 @@ import Card from "../card";
 export default () =>
     <ul style={{ listStyleType: "none" }}>
         <li><a href="/billy-brockmueller-invitational/records">Meet Records</a></li>
-        <li><a href="/billy-brockmueller-invitational/results/2018.pdf">2022</a></li>
+        <li><a href="/billy-brockmueller-invitational/results/2022.pdf">2022</a></li>
         <li><a href="https://tfrrs.org/results/63479/">2020</a></li>
         <li><a href="/billy-brockmueller-invitational/results/2019.pdf">2019</a></li>
         <li><a href="/billy-brockmueller-invitational/results/2018.pdf">2018</a></li>
