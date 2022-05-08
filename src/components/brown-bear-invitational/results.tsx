f9e2d9bb4e32c5d114853e0bcd7e605e8f8e1ef5
Max Heller <max.a.heller@gmail.com>
2022-05-08 08:56:20 -0400

    merge 2021 BBI results

--- src/components/brown-bear-invitational/results.tsx
+++ src/components/brown-bear-invitational/results.tsx
@@ -1,17 +1,7 @@
 import React from "react"
-import Card from "../card";
 
 export default () =>
-    <>
-        <Card title="2021">
-            <a href="/brown-bear-invitational/results/2021/overall-women.html">Overall Women</a>
-            <a href="/brown-bear-invitational/results/2021/overall-men.html">Overall Men</a>
-            <a href="/brown-bear-invitational/results/2021/nirca-women.html">NIRCA Women</a>
-            <a href="/brown-bear-invitational/results/2021/nirca-men.html">NIRCA Men</a>
-            <a href="/brown-bear-invitational/results/2021/usatf-teams-women.html">USATF Teams Women</a>
-            <a href="/brown-bear-invitational/results/2021/usatf-teams-men.html">USATF Teams Men</a>
-        </Card>
-        <Card title="2019">
-            <a href="/brown-bear-invitational/results/2019.txt">Compiled</a>
-        </Card>
-    </>
+    <ul style={{ listStyleType: "none" }}>
+        <li><a href="/brown-bear-invitational/results/2021.txt">2021</a></li>
+        <li><a href="/brown-bear-invitational/results/2019.txt">2019</a></li>
+    </ul>
