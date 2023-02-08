fc4dab6046abc940848c81d6a85dabb1f64b876c
sydrobe <72218993+sydrobe@users.noreply.github.com>
2023-02-08 10:52:57 -0500

    old sheet :(

--- src/components/billy-brockmueller-invitational/order-of-events.tsx
+++ src/components/billy-brockmueller-invitational/order-of-events.tsx
@@ -1,6 +1,6 @@
 import { GSheetsTable } from "../table"
 
-const ORDER_OF_EVENTS_SHEET_ID = "1hHV3PcK2PRhJK40xlhsF4u1aTzTSgyAnAfxKug4NdR0";
+const ORDER_OF_EVENTS_SHEET_ID = "1l2xNdYDhPGymM981DcrBkpUcrMMqUi9Fh8OFEaoqXho";
 const ORDER_OF_EVENTS_SHEET_NAME = "Indoor Schedule";
 
 const OrderOfEvents = () => GSheetsTable({
