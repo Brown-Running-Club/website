6e44b14ca0b54fe9b882d2503234abb18cf8b434
sydrobe <72218993+sydrobe@users.noreply.github.com>
2023-03-05 11:55:06 -0500

    Update brunos-backyard-classic.tsx

--- src/pages/brunos-backyard-classic.tsx
+++ src/pages/brunos-backyard-classic.tsx
@@ -39,7 +39,7 @@ export default () => {
               <b>Location:</b> Brown Stadium, 400 Elmgrove Ave, Providence, RI 02906 <br />
               <br />
               <b>Registration: </b>
-              Registration will be done through /*<a href="https://www.directathletics.com/meets/track/73516.html">*/DirectAthletics (link coming soon).</a>.
+              Registration will be done through DirectAthletics (link coming soon).</a>.
               <br />
               <b>Entry Fees:</b>
               <ul style={{ listStyleType: "none" }}>
