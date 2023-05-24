1c4acf3c424aaac0317acf8293ad835e351565c2
sydrobe <72218993+sydrobe@users.noreply.github.com>
2023-05-24 15:38:42 -0700

    bbi 2022 results archive

--- src/components/brown-bear-invitational/results.tsx
+++ src/components/brown-bear-invitational/results.tsx
@@ -2,6 +2,7 @@ import React from "react"
 
 export default () =>
     <ul style={{ listStyleType: "none" }}>
+        <li><a https://ma.milesplit.com/meets/503874-brown-bear-invitational-2022/results#.YzjSVuzMJpJ">2022</a></li>
         <li><a href="/brown-bear-invitational/results/2021.txt">2021</a></li>
         <li><a href="/brown-bear-invitational/results/2019.txt">2019</a></li>
     </ul>
