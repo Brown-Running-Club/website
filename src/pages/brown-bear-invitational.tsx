05cfdbcdd693baa79bf3216210b8fc2a4761e9b4
aroth7 <77936025+aroth7@users.noreply.github.com>
2023-07-15 12:38:39 -0400

    Updated brown bear prices

--- src/pages/brown-bear-invitational.tsx
+++ src/pages/brown-bear-invitational.tsx
@@ -41,7 +41,7 @@ export default () => {
               </ul>
               <b>Entry Fee:</b> 
               <ul style={{ listStyleType: "none" }}>
-                <li>$12/athlete for youth teams, max $150/gender per team</li>
+                <li>$10/athlete for youth teams, max $150/gender per team</li>
                 <li>$17/athlete for NIRCA clubs and high school teams, max $250/gender</li>
                 <li>$25/athlete for open athletes, max $300/gender</li>
                 <li>$35/athlete for day-of registration</li>
