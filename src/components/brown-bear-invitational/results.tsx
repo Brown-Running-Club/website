20b704215a76096f625fdc447962ffc35964284f
Aidan-Hennessey <95508994+Aidan-Hennessey@users.noreply.github.com>
2023-09-30 21:32:40 -0400

    added 2023 results link

--- src/components/brown-bear-invitational/results.tsx
+++ src/components/brown-bear-invitational/results.tsx
@@ -2,6 +2,7 @@ import React from "react"
 
 export default () =>
     <ul style={{ listStyleType: "none" }}>
+        <li><a href="https://ma.milesplit.com/meets/575226-brown-bear-invitational-2023/results">2023</a></li>
         <li><a href="https://ma.milesplit.com/meets/503874-brown-bear-invitational-2022/results#.YzjSVuzMJpJ">2022</a></li>
         <li><a href="/brown-bear-invitational/results/2021.txt">2021</a></li>
         <li><a href="/brown-bear-invitational/results/2019.txt">2019</a></li>
