a63e615a387aa6d8733173dfb770c37b52508b7e
lchiu6 <103904861+lchiu6@users.noreply.github.com>
2024-02-04 19:47:57 -0500

    Update sprinting.tsx (#73)

--- src/pages/sprinting.tsx
+++ src/pages/sprinting.tsx
@@ -26,3 +26,28 @@ export default () => (
             join our GroupMe!
           </p>
         </Card>
+      </WideContainer>
+    </PageBody>
+  </Layout>
+)
+
+const styles = {
+  infoText: {
+    ...(theme.typography.h4 as any),
+    margin: 0,
+  },
+  link: {
+    color: theme.palette.red,
+    textDecoration: "none" as "none",
+    display: "inline",
+  },
+  bodyText: {
+    ...(theme.typography.body as any),
+    margin: 0,
+  },
+  bodyTextNoButton: {
+    ...(theme.typography.body as any),
+    margin: 0,
+    marginBottom: theme.spacing.unit * 2,
+  },
+}
