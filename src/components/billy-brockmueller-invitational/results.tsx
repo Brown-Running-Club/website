06b2aa36de460d75ec60d93e81c01139f9c31b5d
aroth7 <77936025+aroth7@users.noreply.github.com>
2024-02-25 21:10:00 -0500

    added 2024 results

--- src/components/billy-brockmueller-invitational/results.tsx
+++ src/components/billy-brockmueller-invitational/results.tsx
@@ -3,6 +3,7 @@ import React from "react"
 export default () =>
     <ul style={{ listStyleType: "none" }}>
         <li><a href="/billy-brockmueller-invitational/records">Meet Records</a></li>
+        <li><a href="/billy-brockmueller-invitational/results/2024.pdf">2024</a></li>
         <li><a href="https://ri.milesplit.com/meets/526353-billy-brockmueller-invitational-2023/results#.ZATKJuzMIq0">2023</a></li>
         <li><a href="/billy-brockmueller-invitational/results/2022.pdf">2022</a></li>
         <li><a href="https://tfrrs.org/results/63479/">2020</a></li>
