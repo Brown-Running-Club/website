9805750816b2935688480be9f6fe43b04b955d58
adamfjuma <152454448+adamfjuma@users.noreply.github.com>
2024-05-31 21:02:02 -0400

    Update brown-bear-invitational.tsx
    Corrected year for BBI

--- src/pages/brown-bear-invitational.tsx
+++ src/pages/brown-bear-invitational.tsx
@@ -25,7 +25,7 @@ export default () => {
         <WideContainer>
           <Card>
             <p style={styles.welcomeText}>
-              <b>Date:</b> Saturday, September 28, 2023 <br />
+              <b>Date:</b> Saturday, September 28, 2024 <br />
               <br />
               <b>Location:</b> Highland Park, 104 Mechanic Street, Attleboro, MA
               02703 <br />
