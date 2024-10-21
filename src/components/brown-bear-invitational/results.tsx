70e05d9aadcf7b3fb5dc67eaadb07d80c2b17c47
lchiu6 <103904861+lchiu6@users.noreply.github.com>
2024-10-21 16:59:05 -0700

    Update results.tsx (#93)

--- src/components/brown-bear-invitational/results.tsx
+++ src/components/brown-bear-invitational/results.tsx
@@ -2,6 +2,7 @@ import React from "react"
 
 export default () =>
     <ul style={{ listStyleType: "none" }}>
+        <li><a href="https://ma.milesplit.com/meets/640151-brown-bear-invitational-2024/results">2024</a></li>
         <li><a href="https://ma.milesplit.com/meets/576255-brown-bear-invitational-2023/results">2023</a></li>
         <li><a href="https://ma.milesplit.com/meets/503874-brown-bear-invitational-2022/results#.YzjSVuzMJpJ">2022</a></li>
         <li><a href="/brown-bear-invitational/results/2021.txt">2021</a></li>
