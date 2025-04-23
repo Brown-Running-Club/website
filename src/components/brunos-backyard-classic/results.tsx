bcc8e050f68eb2a5813dc465fd33169b6c2cd9b8
haydengonz <152454397+haydengonz@users.noreply.github.com>
2025-04-22 21:48:18 -0400

    Update results.tsx

--- src/components/brunos-backyard-classic/results.tsx
+++ src/components/brunos-backyard-classic/results.tsx
@@ -6,5 +6,5 @@ export default () =>
         <li><a href="/brunos-backyard-classic/results/2022.html">2022</a></li>
         <li><a href="https://ri.milesplit.com/meets/545463-brunos-backyard-classic-2023/results/911214?type=raw#.ZE8KwOzML9E">2023</a></li>
         <li><a href="/brunos-backyard-classic/results/2024.pdf">2024</a></li>
-        <li><a href="https://ri.milesplit.com/meets/680275-brown-run-club-brunos-backyard-classic-2025/results">2023</a></li>
+        <li><a href="https://ri.milesplit.com/meets/680275-brown-run-club-brunos-backyard-classic-2025/results">2025</a></li>
     </ul>
