7cbf8ff8695c791d59cb265a76fa0264af0b9692
adamfjuma <152454448+adamfjuma@users.noreply.github.com>
2025-12-22 14:16:23 -0500

    Update brunos-backyard-classic.tsx

--- src/pages/brunos-backyard-classic.tsx
+++ src/pages/brunos-backyard-classic.tsx
@@ -48,8 +48,8 @@ export default () => {
               <ul style={{ listStyleType: "none" }}>
                 <li>$20/athlete for college club athletes (NIRCA) and high schoolers</li>
                 <li>$25/athlete for other participants (college varsity athletes, non-collegiate club teams, middle school teams, unattached athletes)</li>
-                <li>$400 max/gender team for college clubs (NIRCA) and high schools</li>
-                <li>$400 max/gender team for other teams and clubs</li>
+                <li>$300 max/gender team for college clubs (NIRCA) and high schools</li>
+                <li>$300 max/gender team for other teams and clubs</li>
               </ul>
               Race day registration will be available for $35 until 10:30 for the 5K, DMR, and 4x100m and 11:00 for all other events. There will be no race day registration for relays
               We are unable to refund entry fees for any reason including inclement weather, meet cancellation, athlete conflict, scratches, or over-payment.
