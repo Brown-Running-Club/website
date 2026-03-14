e4c0108833edcb8d32acd6b84062612b1304f3a1
adamfjuma <152454448+adamfjuma@users.noreply.github.com>
2026-03-14 17:47:36 -0400

    Update results.tsx (#153)

--- src/components/brown-bear-invitational/results.tsx
+++ src/components/brown-bear-invitational/results.tsx
@@ -2,6 +2,7 @@ import React from "react"
 
 export default () =>
     <ul style={{ listStyleType: "none" }}>
+        <li><a href="https://milesplit.live/meets/711273">2025</a></li>
         <li><a href="https://ma.milesplit.com/meets/640151-brown-bear-invitational-2024/results">2024</a></li>
         <li><a href="https://ma.milesplit.com/meets/576255-brown-bear-invitational-2023/results">2023</a></li>
         <li><a href="https://ma.milesplit.com/meets/503874-brown-bear-invitational-2022/results#.YzjSVuzMJpJ">2022</a></li>
