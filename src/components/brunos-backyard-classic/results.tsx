7e56a2b5a7a8a1a186b8c7139b7c04638f11fffb
adamfjuma <152454448+adamfjuma@users.noreply.github.com>
2026-04-17 15:49:51 -0400

    Update results.tsx

--- src/components/brunos-backyard-classic/results.tsx
+++ src/components/brunos-backyard-classic/results.tsx
@@ -3,8 +3,9 @@ import React from "react"
 export default () =>
     <ul style={{ listStyleType: "none" }}>
         <li><a href="/brunos-backyard-classic/records">Meet Records</a></li>
-        <li><a href="/brunos-backyard-classic/results/2022.html">2022</a></li>
-        <li><a href="https://ri.milesplit.com/meets/545463-brunos-backyard-classic-2023/results/911214?type=raw#.ZE8KwOzML9E">2023</a></li>
-        <li><a href="/brunos-backyard-classic/results/2024.pdf">2024</a></li>
+        <li><a href="https://milesplit.live/meets/737020/events/4/results/F/M">2026</a></li>
         <li><a href="https://ri.milesplit.com/meets/680275-brown-run-club-brunos-backyard-classic-2025/results">2025</a></li>
+        <li><a href="/brunos-backyard-classic/results/2024.pdf">2024</a></li>
+        <li><a href="https://ri.milesplit.com/meets/545463-brunos-backyard-classic-2023/results/911214?type=raw#.ZE8KwOzML9E">2023</a></li>
+        <li><a href="/brunos-backyard-classic/results/2022.html">2022</a></li>
     </ul>
